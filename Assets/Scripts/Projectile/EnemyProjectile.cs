36af7f3169b79ab62933b77ab43394e2c419443a
AlistairArk <AlistairArk@Hotmail.com>
2021-06-12 11:47:31 +0100

    Update EnemyProjectile.cs

--- Assets/Scripts/Projectile/EnemyProjectile.cs
+++ Assets/Scripts/Projectile/EnemyProjectile.cs
@@ -8,6 +8,20 @@ public class EnemyProjectile : MonoBehaviour
 
     public void Start () { 
 
-        Destroy(gameObject, destroyTime); // Destory after seconds
+        Destroy(gameObject, destroyTime); // Destroy after seconds
+    }
+
+
+    // Collision detection
+    void OnCollisionEnter(Collision collision)
+    {
+        // Projectile collision with player
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerStats PlayerStats = collision.gameObject.GetComponent<PlayerStats>();
+            PlayerStats.hp -= 1;
+
+            Destroy(gameObject); // Destroy self
+        }
     }
 }
