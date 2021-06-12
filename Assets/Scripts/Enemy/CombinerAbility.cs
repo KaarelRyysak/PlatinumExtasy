ebfade35d1e380925aa600d08f51add45f90a708
Cade Anderson <cadeanderson99@gmail.com>
2021-06-12 15:15:27 -0600

    Rename Enemy movement script

---
+++ Assets/Scripts/Enemy/CombinerAbility.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombinerAbility : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
