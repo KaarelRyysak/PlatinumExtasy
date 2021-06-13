506c0bff8dd501fd56da77a2a1f6a3ed1e25fbcd
Kaarel Ryysak <kaarel@ryysak.com>
2021-06-13 12:51:51 +0300

    score awarded on enemy death

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class EnemyStats : MonoBehaviour
 {
-    public float health = 4;
+    public float health = 4f;
+    public float scoreAwardedOnDeath = 4f;
 
     public void TakeDamage(float damage)
     {
         health -= damage;
         if (health <= 0f)
         {
+            GameManager gameManager = FindObjectOfType<GameManager>();
+            gameManager.stats.score += scoreAwardedOnDeath;
             Destroy(this.gameObject);
         }
     }
